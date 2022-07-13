18f3d3a8eb5c4e9ab80da869f9ba21a7f24f24b8
roynaldi19 <roynaldi19@gmail.com>
2022-07-13 11:19:08 +0700

    Add user interactivity

--- app/src/main/java/com/roynaldi19/akf02_01aboutme/MainActivity.kt
+++ app/src/main/java/com/roynaldi19/akf02_01aboutme/MainActivity.kt
@@ -1,11 +1,52 @@
 package com.roynaldi19.akf02_01aboutme
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import com.roynaldi19.akf02_01aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.doneButton.setOnClickListener {
+            addNickName(it)
+        }
+
+        binding.nicknameText.setOnClickListener {
+            updateNickName(it)
+        }
+
+    }
+
+    private fun addNickName(view: View) {
+        binding.nicknameText.text = binding.nicknameEdit.text
+        binding.nicknameEdit.visibility = View.GONE
+        view.visibility = View.GONE
+        binding.nicknameText.visibility = View.VISIBLE
+
+        //hide keyboard
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+
+    }
+
+    private fun updateNickName(view: View) {
+        binding.nicknameEdit.visibility = View.VISIBLE
+        binding.doneButton.visibility = View.VISIBLE
+        view.visibility = View.GONE
+
+        binding.nicknameEdit.requestFocus()
+
+        //hide keyboard
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+
     }
 }
